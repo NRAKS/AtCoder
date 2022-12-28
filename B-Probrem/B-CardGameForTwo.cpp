83acb10e144c5ce6e3f12f34df2b7dfe6cc3d5b8
NRAKS <noriaki.sonota@gmail.com>
2022-12-29 02:12:24 +0900

    2022/12/29

--- B-Probrem/B-CardGameForTwo.cpp
+++ B-Probrem/B-CardGameForTwo.cpp
@@ -3,33 +3,31 @@
 using namespace std;
 
 int main() {
-    int N, M, money=0, money_max=0;
+    int N, Alice=0, Bob=0, max=0, max_position=0;
     cin >> N;
-    vector<string> vec_N(N);
+    int N_origin = N;
+    vector<int> vec_N(N);
 
     for (int i=0; i<N; i++){
         cin >> vec_N.at(i);
     }
-    cin >> M;
-    vector<string> vec_M(M);
-    for (int i=0; i<M; i++){
-        cin >> vec_M.at(i);
-    }
-    
-    for(int i=0; i<N; i++){
-        money=0;
-        for(int x=0; x<N; x++){
-            if (vec_N.at(i) == vec_N.at(x))
-                money+=1;
-        }
-        for(int x=0; x<M; x++){
-            if(vec_N.at(i) == vec_M.at(x)){
-                money-=1;
+    for (int i=0; i<N_origin; i++){
+        max = 0;
+        for (int j=0; j<N; j++){
+            if(max < vec_N.at(j)){
+                max = vec_N.at(j);
+                max_position = j;
             }
         }
-        if (money_max < money)
-            money_max = money;
+        if (i%2 ==0){
+            Alice += max;
+        }
+        else{
+            Bob+= max;
+        }
+        vec_N.erase(vec_N.begin()+max_position);
+        N-=1;
     }
 
-    cout << money_max << endl;
+    cout << Alice - Bob << endl;
 }
